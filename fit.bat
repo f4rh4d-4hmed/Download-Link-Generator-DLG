86a694236a554af346d0cf22d2c66386b34b7d5f
Farhad Ahmed <161406872+f4rh4d-4hmed@users.noreply.github.com>
2024-04-05 15:32:32 +0600

    Add files via upload
    Added Files For Windows users

---
+++ fit.bat
@@ -0,0 +1,5 @@
+@echo off
+title "Farhad is opening program for you"
+cd /d "%~dp0" 
+start "" "Fitgirl-Repack.exe" 
+exit
---
+++ kat.bat
@@ -0,0 +1,5 @@
+@echo off
+title "Farhad is opening program for you"
+cd /d "%~dp0" 
+start "" "kat.exe" 
+exit
